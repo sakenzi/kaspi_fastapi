9cb77533380c3172d983e8e54fa81421b878572c
sakenzi <ermekulysaken8@gmail.com>
2025-06-09 01:10:41 +0600

    Обновлено celery

--- app/api/comparisons/celery/celery_app.py
+++ app/api/comparisons/celery/celery_app.py
@@ -1,6 +1,11 @@
+
 from celery import Celery
-# from celery.signals import worker_process_init
-# from app.api.comparisons.celery.tasks import parse_kaspi_products
+from celery.signals import worker_process_init, worker_ready
+import logging
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 celery = Celery(
     "kaspi_parser",
@@ -18,9 +23,25 @@ celery.conf.beat_schedule = {
 
 celery.conf.timezone = 'UTC'
 
-# @worker_process_init
-# def on_worket_init(**kwargs):
-#     task = parse_kaspi_products.delay()
+@worker_process_init.connect
+def on_worker_process_init(**kwargs):
+    logger.info("Сигнал worker_process_init сработал")
+    try:
+        from app.api.comparisons.celery.tasks import parse_kaspi_products
+        task = parse_kaspi_products.delay()
+        logger.info(f"Запущена задача parse_kaspi_products с ID: {task.id}")
+    except Exception as e:
+        logger.error(f"Ошибка при запуске parse_kaspi_products в worker_process_init: {str(e)}")
+
+@worker_ready.connect
+def on_worker_ready(**kwargs):
+    logger.info("Сигнал worker_ready сработал")
+    try:
+        from app.api.comparisons.celery.tasks import parse_kaspi_products
+        task = parse_kaspi_products.delay()
+        logger.info(f"Запущена задача parse_kaspi_products с ID: {task.id}")
+    except Exception as e:
+        logger.error(f"Ошибка при запуске parse_kaspi_products в worker_ready: {str(e)}")
 
 # if __name__ == "__main__":
 #     celery.start()
